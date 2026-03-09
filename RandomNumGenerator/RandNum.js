b4e1e82833504bb7395584f8c413680f1f6e1569
Naydiam <naydiam42@gmail.com>
2026-03-08 20:42:03 -0500

    Delete RandomNumGenerator directory

--- RandomNumGenerator/RandNum.js
+++
@@ -1,9 +0,0 @@
-const label = document.getElementById("mylabel");
-const button = document.getElementById("butts");
-const min = 1;
-const max =6;
-let randnum;
-button.onclick = function(){
-    randnum = Math.floor(Math.random() * max)+min;
-    label.textContent = "You rolled a " + randnum + "!";
-}
