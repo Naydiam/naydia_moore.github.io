7195ee760617b7d43793169dd7788ab9d9df4396
Naydiam <naydiam42@gmail.com>
2026-03-08 20:41:44 -0500

    Delete JS_practice directory

--- JS_practice/DiceRoller/index.js
+++
@@ -1,6 +0,0 @@
-function RollMe()
-{
-    const numOfDice = document.getElementById("numOfDice").value;
-    const diceResult = document.getElementById("Result");
-    const diceImages = document.getElementById("Images");
-}
